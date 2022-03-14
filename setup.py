fe984cdfa9b066ba79a99832a685198338d06339
Gergo Nyiro <gergo@nyiro.name>
2022-03-14 16:06:10 +0100

    setup.py: version 0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as fp:
 
 setup(
     name="gitlogfeed",
-    version="0.1.0",
+    version="0.1.1",
     description="Create an atom feed from git log",
     long_description=long_description,
     long_description_content_type="text/markdown",
