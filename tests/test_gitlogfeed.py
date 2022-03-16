18057e29a455f80b962783018ad5ab4deb50f0ab
Gergo Nyiro <gergo@nyiro.name>
2022-03-16 20:57:47 +0100

    tests: test_git_log

--- tests/test_gitlogfeed.py
+++ tests/test_gitlogfeed.py
@@ -15,3 +15,19 @@ def test_git_show(tmpdir):
 
     assert git.show("HEAD", "%s") == "commit title"
     assert git.show("HEAD", "%b") == "commit message\n"
+
+
+def test_git_log(tmpdir):
+    repo = tmpdir.mkdir("repo")
+
+    with repo.as_cwd():
+        subprocess.check_call(["git", "init"])
+        repo.join("foo.py").write_text("print(42)", "ascii")
+        subprocess.check_call(["git", "add", "."])
+        subprocess.check_call(["git", "commit", "-m", "first commit"])
+        repo.join("foo.py").write_text("print(24)", "ascii")
+        subprocess.check_call(["git", "commit", "-a", "-m", "second commit"])
+
+    git = Git(str(repo), None, 20)
+
+    assert git.log(2, "%s", list) == ["second commit\n", "first commit"]
