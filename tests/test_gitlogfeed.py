585a4fd5ccd1018aa344bc711f38b697775ab575
Gergo Nyiro <gergo@nyiro.name>
2022-03-17 21:07:07 +0100

    tests: test_git_log_filter

--- tests/test_gitlogfeed.py
+++ tests/test_gitlogfeed.py
@@ -6,10 +6,14 @@ def test_git_show(tmpdir):
     repo = tmpdir.mkdir("repo")
 
     with repo.as_cwd():
-        subprocess.check_call(["git", "init"])
-        repo.join("foo.py").write_text("print(42)", "ascii")
-        subprocess.check_call(["git", "add", "."])
-        subprocess.check_call(["git", "commit", "-m", "commit title\n\ncommit message"])
+        _git_init()
+        _git_commit(
+            repo,
+            "commit title\n\ncommit message",
+            {
+                "foo.py": "print(42)"
+            }
+        )
 
     git = Git(str(repo), None, 20)
 
@@ -22,12 +26,60 @@ def test_git_log(tmpdir):
 
     with repo.as_cwd():
         subprocess.check_call(["git", "init"])
-        repo.join("foo.py").write_text("print(42)", "ascii")
-        subprocess.check_call(["git", "add", "."])
-        subprocess.check_call(["git", "commit", "-m", "first commit"])
-        repo.join("foo.py").write_text("print(24)", "ascii")
-        subprocess.check_call(["git", "commit", "-a", "-m", "second commit"])
+        _git_init()
+        _git_commit(
+            repo,
+            "first commit",
+            {
+                "foo.py": "print(42)"
+            }
+        )
+        _git_commit(
+            repo,
+            "second commit",
+            {
+                "foo.py": "print(24)"
+            }
+        )
 
     git = Git(str(repo), None, 20)
 
     assert git.log(2, "%s", list) == ["second commit\n", "first commit"]
+
+
+def test_git_log_filter(tmpdir):
+    repo = tmpdir.mkdir("repo")
+
+    with repo.as_cwd():
+        subprocess.check_call(["git", "init"])
+        _git_init()
+        _git_commit(
+            repo,
+            "python commit",
+            {
+                "foo.py": "print(42)"
+            }
+        )
+        _git_commit(
+            repo,
+            "php commit",
+            {
+                "foo.php": "echo 42;"
+            }
+        )
+
+    git = Git(str(repo), "*.py", 20)
+
+    assert git.log(2, "%s", list) == ["python commit"]
+
+
+def _git_init():
+    subprocess.check_call(["git", "init"])
+
+
+def _git_commit(repo, message, files):
+    for filename, content in files.items():
+        repo.join(filename).write_text(content, "ascii")
+        subprocess.check_call(["git", "add", filename])
+
+    subprocess.check_call(["git", "commit", "-m", message])
